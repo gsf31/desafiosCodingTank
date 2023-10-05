7cf8870191713e55d8019892b9698dbc35432c41
Gus <gustavosodrefigueiredo1@gmail.com>
2023-10-05 07:42:41 -0300

    Desafio 3 - desafio de calcular porcentagem de gorjeta. Coloquei um Easter Egg para os mãos de vaca hihi

---
+++ Desafio-3/Desafio-3/Desafio-3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace desafio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite o valor total da conta: ");
+                double valorConta = double.Parse(Console.ReadLine());
+
+                Console.Write("Você deseja dar gorjeta de 15% ou 20%: ");
+                double porcentagemGorjeta = double.Parse(Console.ReadLine());
+                if (porcentagemGorjeta == 0)
+                {
+                    Console.WriteLine("Obrigado. Volte sempre!");
+                }
+                else
+                {
+                    double valorGorjeta = (valorConta * porcentagemGorjeta) / 100.0;
+                    double totalPago = valorConta + valorGorjeta;
+
+                    Console.WriteLine($"Valor da gorjeta: R$ {valorGorjeta}");
+                    Console.WriteLine($"Total a ser pago: R$ {totalPago}");
+                }           
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Entrada inválida. Certifique-se de digitar valores numéricos.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+            }
+        }
+    }
+}
+
