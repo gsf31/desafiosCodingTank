571a8f484115792f8d2e5da833b002bfd9386ef3
Gus <gustavosodrefigueiredo1@gmail.com>
2023-10-05 08:02:12 -0300

    Desafio 4 - fui ver a cotação e me doeu na alma, porém está ai o programa que me causou sentimentos de tristeza e pobreza. Entretanto, estudando para mudar isso xD

---
+++ Desafio-4/Desafio-4/Desafio-4/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace desafio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite o valor em reais: ");
+                double valorReais = double.Parse(Console.ReadLine());
+
+                double taxaDolar = 5.16; //Tá alto de novo ;-;
+                double taxaEuro = 5.46; //Tá difícil ir para a Europa '-'
+
+                double valorDolar = valorReais / taxaDolar;
+                double valorEuro = valorReais / taxaEuro;
+
+                Console.WriteLine($"Valor em dólares: {valorDolar:F2}");
+                Console.WriteLine($"Valor em euros: {valorEuro:F2}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Esse programa só aceita valores numéricos.");
+            }
+        }
+    }
+}
+
