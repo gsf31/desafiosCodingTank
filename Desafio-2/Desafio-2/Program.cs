d698b1b2acdb51483b80b795ffb6f611b1ac2cfe
Gus <gustavosodrefigueiredo1@gmail.com>
2023-10-05 07:18:53 -0300

    Desafio 2 - calcula idade (coloquei alguns testes automatizados para saber se o usuário digitou corretamente os dados)

---
+++ Desafio-2/Desafio-2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace desafio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite o ano atual: ");
+                int anoAtual = int.Parse(Console.ReadLine());
+
+                Console.Write("Digite o ano do seu nascimento: ");
+                int anoNascimento = int.Parse(Console.ReadLine());
+
+                if (anoAtual < anoNascimento)
+                {
+                    throw new Exception("O ano atual não pode ser menor que o ano de nascimento");
+                }
+
+                int idade = anoAtual - anoNascimento;
+
+                Console.WriteLine($"Você tem {idade} anos.");
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Esse programa só aceita valores númericos.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Erro: {ex.Message}");
+            }
+        }
+    }
+}
